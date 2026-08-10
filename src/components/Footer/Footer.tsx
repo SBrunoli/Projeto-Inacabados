20b470ca45ee95de69010f7ece5739915d19e7bc
Bruno de Oliveira <brunind2006@gmail.com>
2026-08-10 20:19:00 -0300

    refactor(Footer): organiza componente e estilos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className={` section ${styles.footer}`}>
       <p>inacabados</p>
 
       <p>
