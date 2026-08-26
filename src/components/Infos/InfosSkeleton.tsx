c8aede17a6e5b750a4ca0f0858ebb6a69c8b20c1
guts <brunind2006@gmail.com>
2026-08-26 13:17:15 -0300

    refactor: extrai animação e cor comuns dos skeletons para classe global

--- src/components/Infos/InfosSkeleton.tsx
+++ src/components/Infos/InfosSkeleton.tsx
@@ -5,8 +5,8 @@ function InfosSkeleton() {
     <div className={styles.infosSkeleton}>
       {Array.from({ length: 4 }).map((_, index) => (
         <div key={index} className={styles.infosSkeleton__box}>
-          <div className={styles.infosSkeleton__number}></div>
-          <div className={styles.infosSkeleton__label}></div>
+          <div className={`skeleton ${styles.infosSkeleton__number}`}></div>
+          <div className={`skeleton ${styles.infosSkeleton__label}`}></div>
         </div>
       ))}
     </div>
--- src/components/ProjectsCards/ProjectCardSkeleton.tsx
+++ src/components/ProjectsCards/ProjectCardSkeleton.tsx
@@ -4,19 +4,19 @@ function ProjectCardSkeleton() {
   return (
     <div className={styles.skeletonCard}>
       <div className={styles.skeletonCard__top}>
-        <div className={styles.skeletonCard__category}></div>
-        <div className={styles.skeletonCard__stop}></div>
+        <div className={`skeleton ${styles.skeletonCard__category}`}></div>
+        <div className={`skeleton ${styles.skeletonCard__stop}`}></div>
       </div>
 
-      <div className={styles.skeletonCard__title}></div>
+      <div className={`skeleton ${styles.skeletonCard__title}`}></div>
 
-      <div className={styles.skeletonCard__reason}></div>
-      <div className={styles.skeletonCard__reasonShort}></div>
+      <div className={`skeleton ${styles.skeletonCard__reason}`}></div>
+      <div className={`skeleton ${styles.skeletonCard__reasonShort}`}></div>
 
-      <div className={styles.skeletonCard__progressBar}></div>
+      <div className={`skeleton ${styles.skeletonCard__progressBar}`}></div>
 
       <div className={styles.skeletonCard__bottom}>
-        <div className={styles.skeletonCard__author}></div>
+        <div className={`skeleton ${styles.skeletonCard__author}`}></div>
       </div>
     </div>
   );
